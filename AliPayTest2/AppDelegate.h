03824752c4461978de1951bea13f6b4baf22f435
MartinLi841538513 <841538513@qq.com>
2014-11-17 16:46:01 +0800

    Initial Commit

---
+++ AliPayTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliPayTest2
+//
+//  Created by Gao Huang on 14-11-17.
+//  Copyright (c) 2014年 GL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AliPayTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliPayTest2
+//
+//  Created by Gao Huang on 14-11-17.
+//  Copyright (c) 2014年 GL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
